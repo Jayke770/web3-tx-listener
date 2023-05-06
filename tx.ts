085f4aea5b7f2573c6f663c540cbf59221082832
Jake Velasco <59675743+Jayke770@users.noreply.github.com>
2023-05-06 11:49:36 +0800

    first commit

---
+++ tx.ts
@@ -0,0 +1,22 @@
+import Web3 from "web3"
+let lasttxHash: string
+class TxChecker {
+    private web3: Web3
+    constructor() {
+        this.web3 = new Web3('https://eth.llamarpc.com')
+    }
+    async check() {
+        const block = await this.web3.eth.getBlock("latest")
+        if (block && block?.transactions) {
+            for (let txHash of block.transactions) {
+                const tx = await this.web3.eth.getTransaction(txHash)
+                if (tx && lasttxHash !== tx?.hash) {
+                    lasttxHash = txHash
+                    console.log(tx.hash, tx.from, tx.to, this.web3.utils.fromWei(tx.value, 'ether'), tx.blockNumber)
+                }
+            }
+        }
+    }
+}
+const txns = new TxChecker()
+setInterval(() => txns.check(), 1000)
