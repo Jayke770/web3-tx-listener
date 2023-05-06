6b899f677245645bc105006c5df6e7119325a53b
Jake Velasco <59675743+Jayke770@users.noreply.github.com>
2023-05-06 11:51:46 +0800

    Update

--- tx.ts
+++ tx.ts
@@ -5,17 +5,23 @@ class TxChecker {
     constructor() {
         this.web3 = new Web3('https://eth.llamarpc.com')
     }
-    async check() {
-        const block = await this.web3.eth.getBlock("latest")
-        if (block && block?.transactions) {
-            for (let txHash of block.transactions) {
-                const tx = await this.web3.eth.getTransaction(txHash)
-                if (tx && lasttxHash !== tx?.hash) {
-                    lasttxHash = txHash
-                    console.log(tx.hash, tx.from, tx.to, this.web3.utils.fromWei(tx.value, 'ether'), tx.blockNumber)
+    check() {
+        return new Promise(async (resolve, reject) => {
+            try {
+                const block = await this.web3.eth.getBlock("latest")
+                if (block && block?.transactions) {
+                    for (let txHash of block.transactions) {
+                        const tx = await this.web3.eth.getTransaction(txHash)
+                        if (tx && lasttxHash !== tx?.hash) {
+                            lasttxHash = txHash
+                            console.log(tx.hash, tx.from, tx.to, this.web3.utils.fromWei(tx.value, 'ether'), tx.blockNumber)
+                        }
+                    }
                 }
+            } catch (e) {
+                console.log("Error", e)
             }
-        }
+        })
     }
 }
 const txns = new TxChecker()
